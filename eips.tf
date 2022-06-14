71cab25379da0289fd55352f7c4525bc5b5c1e49
sivad1648 <95473996+sivad1648@users.noreply.github.com>
2022-06-14 20:32:58 +0530

    Update eips.tf

--- eips.tf
+++ eips.tf
@@ -1,7 +1,7 @@
 resource "aws_eip" "nat1" {
 # EIP may result IGW to exist prior to association.
 # Use depends_on to set an explicit dependency on the IGW.
-  depends_on                = [aws_internet_gateway.main]
+  depends_on = [aws_internet_gateway.main]
 }
 
 # A map of tags to assign to the resource.
@@ -12,7 +12,7 @@ resource "aws_eip" "nat1" {
 resource "aws_eip" "nat2" {
 # EIP may result IGW to exist prior to association.
 # Use depends_on to set an explicit dependency on the IGW.
-  depends_on                = [aws_internet_gateway.main]
+  depends_on = [aws_internet_gateway.main]
 }
  
 
